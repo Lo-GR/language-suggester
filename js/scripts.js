036012ba2b00d057c91b66cc14ce43ec887c9fec
Lo-GR <diamondintheroth@gmail.com>
2021-01-15 11:12:58 -0800

    add functionality of calculating results and tie to results button

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-
+// Business Logic
+function calculation(value1, value2, value3, value4, value5){
+  return value1 + value2 + value3 + value4 + value5;
+};
 
 
 
 
 //User logic
 $(document).ready(function(){
-
   $("#startButton").on("click",function(){
     $(this).hide();
     $("#startPage").hide();
@@ -13,6 +15,13 @@ $(document).ready(function(){
     $("#resultsButton").show();
   })
   $("#resultsButton").on("click",function(){
+    const value1 = parseInt($("#question1").val());
+    const value2 = parseInt($("#question2").val());
+    const value3 = parseInt($("#question3").val());
+    const value4 = parseInt($("#question4").val());
+    const value5 = parseInt($("#question5").val());
+    const calcresult = calculation(value1, value2, value3, value4, value5);
+    $("#resultsPage").text(calcresult);
     $(this).hide();
     $("#questionsPage").hide();
     $("#resultsPage").show();
